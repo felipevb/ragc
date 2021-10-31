25d66a8aaa9a9a6ff990a98a37b392e9d62c95c5
Felipe V <11216465+felipevb@users.noreply.github.com>
2021-10-31 12:09:12 -0700

    Adding a Rope Core package to make loading rope core easier and more programmatic.

---
+++ ragc-ropes/src/lib.rs
@@ -0,0 +1,13 @@
+#![no_std]
+
+const ROM_BANKS_NUM: usize = 36;
+const ROM_BANK_NUM_WORDS: usize = 1024;
+
+macro_rules! include_transmute {
+    ($file:expr) => {
+        &core::mem::transmute(*include_bytes!($file))
+    };
+}
+
+pub static LUMINARY131_ROPE: &'static [[u16; ROM_BANK_NUM_WORDS]; ROM_BANKS_NUM] = unsafe { include_transmute!("../res/LUMINARY131.ROM") };
+pub static BLANK_ROPE: &'static [[u16; ROM_BANK_NUM_WORDS]; ROM_BANKS_NUM] = &[[0; ROM_BANK_NUM_WORDS]; ROM_BANKS_NUM];
