33f639e22af2e33b763167fd0e63890f7152a107
Felipe V <11216465+felipevb@users.noreply.github.com>
2021-11-06 05:02:24 -0700

    Adding more rope cores for testing

--- ragc-ropes/src/lib.rs
+++ ragc-ropes/src/lib.rs
@@ -8,6 +8,7 @@ macro_rules! include_transmute {
         &core::mem::transmute(*include_bytes!($file))
     };
 }
-
+pub static RETREAD50_ROPE: &'static [[u16; ROM_BANK_NUM_WORDS]; ROM_BANKS_NUM] = unsafe { include_transmute!("../res/RETREAD50.ROM") };
+pub static VALIDATION_ROPE: &'static [[u16; ROM_BANK_NUM_WORDS]; ROM_BANKS_NUM] = unsafe { include_transmute!("../res/VALIDATION.ROM") };
 pub static LUMINARY131_ROPE: &'static [[u16; ROM_BANK_NUM_WORDS]; ROM_BANKS_NUM] = unsafe { include_transmute!("../res/LUMINARY131.ROM") };
 pub static BLANK_ROPE: &'static [[u16; ROM_BANK_NUM_WORDS]; ROM_BANKS_NUM] = &[[0; ROM_BANK_NUM_WORDS]; ROM_BANKS_NUM];
