355fea921a0554729bd14d55e2e0c23b000329c4
Felipe V <11216465+felipevb@users.noreply.github.com>
2022-07-13 08:07:15 -0700

    Modified baseline to include rp2040-hal given embassy does not have the USB drivers for the RP2040.
    Embassy is building their own PAC and does not have support for the USB endpoint. Testing whether I can use both at the same time.
    
    Initial gameplan is to use embassy for Task management and baseline bring-up, then use RP2040-hal for PAC related work. Going to be interesting.

--- ragc-targets/rp2040/src/main.rs
+++ ragc-targets/rp2040/src/main.rs
@@ -4,22 +4,44 @@
 
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
-use embassy_rp::{gpio, Peripherals};
-use gpio::{Level, Output};
+use embassy_rp::Peripherals;
+//use gpio::{Level, Output};
 use panic_probe as _;
+
+use rp2040_hal as hal;
+use hal::pac;
+
+use embedded_hal::digital::v2::OutputPin;
 
 mod tasks;
 
 #[embassy::main]
 async fn main(spawner: Spawner, p: Peripherals) {
-    let mut led = Output::new(p.PIN_25, Level::Low);
 
+    // Grab our singleton objects
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    //let mut led = Output::new(p.PIN_25, Level::Low);
     tasks::agc::init_agc_tasks(&spawner);
-    loop {
-        led.set_high();
-        Timer::after(Duration::from_secs(1)).await;
 
-        led.set_low();
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // Configure GPIO25 as an output
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        Timer::after(Duration::from_secs(1)).await;
+        led_pin.set_low().unwrap();
         Timer::after(Duration::from_secs(1)).await;
     }
 }
