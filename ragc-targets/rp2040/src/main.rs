cfd512df73c44b242aa9955dade815a77ab92e03
Felipe V <11216465+felipevb@users.noreply.github.com>
2022-07-13 08:16:07 -0700

    Adding USB serial code. Had to add clocks line which I think won't work, but we will see.

--- ragc-targets/rp2040/src/main.rs
+++ ragc-targets/rp2040/src/main.rs
@@ -5,11 +5,15 @@
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
 use embassy_rp::Peripherals;
+use hal::clocks::ClocksManager;
 //use gpio::{Level, Output};
 use panic_probe as _;
 
 use rp2040_hal as hal;
 use hal::pac;
+
+use usb_device::{class_prelude::*, prelude::*};
+use usbd_serial::SerialPort;
 
 use embedded_hal::digital::v2::OutputPin;
 
@@ -36,12 +40,58 @@ async fn main(spawner: Spawner, p: Peripherals) {
         &mut pac.RESETS,
     );
 
+    // Set up the USB driver
+    let clocks = ClocksManager::new(pac.CLOCKS);
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    // Set up the USB Communications Class Device driver
+    let mut serial = SerialPort::new(&usb_bus);
+
+    // Create a USB device with a fake VID and PID
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+
+
     // Configure GPIO25 as an output
     let mut led_pin = pins.gpio25.into_push_pull_output();
     loop {
-        led_pin.set_high().unwrap();
-        Timer::after(Duration::from_secs(1)).await;
-        led_pin.set_low().unwrap();
-        Timer::after(Duration::from_secs(1)).await;
+        if usb_dev.poll(&mut [&mut serial]) {
+            let mut buf = [0u8; 64];
+            match serial.read(&mut buf) {
+                Err(_e) => {
+                    // Do nothing
+                }
+                Ok(0) => {
+                    // Do nothing
+                }
+                Ok(count) => {
+                    // Convert to upper case
+                    buf.iter_mut().take(count).for_each(|b| {
+                        b.make_ascii_uppercase();
+                    });
+                    // Send back to the host
+                    let mut wr_ptr = &buf[..count];
+                    while !wr_ptr.is_empty() {
+                        match serial.write(wr_ptr) {
+                            Ok(len) => wr_ptr = &wr_ptr[len..],
+                            // On error, just drop unwritten data.
+                            // One possible error is Err(WouldBlock), meaning the USB
+                            // write buffer is full.
+                            Err(_) => break,
+                        };
+                    }
+                }
+            }
+        }
     }
 }
