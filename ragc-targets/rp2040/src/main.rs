d40842180d7a36e1e53615efec0a3228daf6ced1
Felipe V <11216465+felipevb@users.noreply.github.com>
2022-07-17 09:42:19 -0700

    Major restructure. Comments below
    main.rs:
    - Moved USB code to separate task
    - Added LED pin to global static to start
    
    engine.rs:
    - Remove file since empty file

--- ragc-targets/rp2040/src/main.rs
+++ ragc-targets/rp2040/src/main.rs
@@ -4,94 +4,26 @@
 
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
+use embassy::util::Forever;
 use embassy_rp::Peripherals;
-use hal::clocks::ClocksManager;
-//use gpio::{Level, Output};
+use embassy_rp::gpio::{Output, Level};
+
+use embassy_rp::peripherals::PIN_25;
 use panic_probe as _;
-
-use rp2040_hal as hal;
-use hal::pac;
-
-use usb_device::{class_prelude::*, prelude::*};
-use usbd_serial::SerialPort;
-
-use embedded_hal::digital::v2::OutputPin;
 
 mod tasks;
 
+pub static LED: Forever<Output<PIN_25>> = Forever::new();
+
 #[embassy::main]
 async fn main(spawner: Spawner, p: Peripherals) {
+    let led = Output::new(p.PIN_25, Level::Low);
+    LED.put(led);
 
-    // Grab our singleton objects
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-
-    //let mut led = Output::new(p.PIN_25, Level::Low);
     tasks::agc::init_agc_tasks(&spawner);
+    tasks::serial::init_usb_serial_task(&spawner);
 
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
-
-    // Set the pins to their default state
-    let pins = hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-
-    // Set up the USB driver
-    let clocks = ClocksManager::new(pac.CLOCKS);
-    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
-        pac.USBCTRL_REGS,
-        pac.USBCTRL_DPRAM,
-        clocks.usb_clock,
-        true,
-        &mut pac.RESETS,
-    ));
-
-    // Set up the USB Communications Class Device driver
-    let mut serial = SerialPort::new(&usb_bus);
-
-    // Create a USB device with a fake VID and PID
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("Fake company")
-        .product("Serial port")
-        .serial_number("TEST")
-        .device_class(2) // from: https://www.usb.org/defined-class-codes
-        .build();
-
-
-    // Configure GPIO25 as an output
-    let mut led_pin = pins.gpio25.into_push_pull_output();
     loop {
-        if usb_dev.poll(&mut [&mut serial]) {
-            let mut buf = [0u8; 64];
-            match serial.read(&mut buf) {
-                Err(_e) => {
-                    // Do nothing
-                }
-                Ok(0) => {
-                    // Do nothing
-                }
-                Ok(count) => {
-                    // Convert to upper case
-                    buf.iter_mut().take(count).for_each(|b| {
-                        b.make_ascii_uppercase();
-                    });
-                    // Send back to the host
-                    let mut wr_ptr = &buf[..count];
-                    while !wr_ptr.is_empty() {
-                        match serial.write(wr_ptr) {
-                            Ok(len) => wr_ptr = &wr_ptr[len..],
-                            // On error, just drop unwritten data.
-                            // One possible error is Err(WouldBlock), meaning the USB
-                            // write buffer is full.
-                            Err(_) => break,
-                        };
-                    }
-                }
-            }
-        }
+        Timer::after(Duration::from_secs(5)).await;
     }
 }
